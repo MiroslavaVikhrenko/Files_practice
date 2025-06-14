c1140cbc27758cc4cf4bd153e1cbdc09e05e9042
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-06-14 14:38:53 -0600

    Practice working with files

---
+++ 20250614_Task8/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _20250614_Task8
+{
+    /*
+     Дан текстовый файл, где каждая строка начинается на новой линии и может начинаться как с заглавной, 
+    так и прописной буквы. Сделать заглавной каждую букву нового предложения, используя методы расширения Linq 
+    и собственный метод расширения для класса String.
+     */
+
+ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\mvikh\OneDrive\Desktop\test.txt";
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("The file does not exist.");
+                return;
+            }
+
+            var lines = File.ReadAllLines(path);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nOriginal text:\n");
+            Console.ResetColor();
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            var modifiedLines = lines
+                .Select(line => line.CapitalizeFirstLetter())
+                .ToList();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nModified text:\n");
+            Console.ResetColor();
+            PrintText(modifiedLines);
+
+            Console.ReadKey();
+        }
+
+        public static void PrintText(List<string> text)
+        {
+            foreach (var line in text)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ 20250614_Task8/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20250614_Task8
+{
+    public static class StringExtensions
+    {
+        public static string CapitalizeFirstLetter(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return input;
+
+            input = input.TrimStart(); // remove leading spaces
+
+            if (char.IsLetter(input[0]))
+            {
+                return char.ToUpper(input[0]) + input.Substring(1);
+            }
+
+            return input;
+        }
+    }
+}
